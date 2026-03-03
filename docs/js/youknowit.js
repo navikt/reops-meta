e9badcdec7080a0260c3d96808fa43b8b7fcc1e7
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-03-03 22:06:57 +0100

    feat: tracking

---
+++ docs/js/youknowit.js
@@ -0,0 +1,8 @@
+(function () {
+    var script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://cdn.nav.no/team-researchops/sporing/sporing.js";
+    script.dataset.hostUrl = "https://reops-event-proxy.ekstern.dev.nav.no";
+    script.dataset.websiteId = "e3baefc1-c576-4f62-a7c2-9db2dcd13dca";
+    document.head.appendChild(script);
+})();
